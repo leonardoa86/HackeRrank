6e9a4f1dc37bca5d537033470c9e0d7827f9a9e5
Leonardo Lima <leoamadori86@gmail.com>
2017-01-18 12:13:45 -0600

    Create staircase_two.c
    Same as the other stair but this one prints numbers.

---
+++ staircase_two.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int n, i, k, j;
+    int num;
+
+    printf("Enter number of rows\n");
+    scanf("%d",&n);
+
+    for ( i = 0 ; i < n ; i++ )
+    {
+        //Top part of the triangle
+        for( j = 0 ; j < n-i -1; j++ )
+            printf(" ");
+        //Bottom part of the triangle
+        for(k = 0 ; k < i; k++ ){
+            num = n-j-1;
+            printf("%d",num);
+        }
+    printf("\n");
+    }
+    getch();
+    return 0;
+}
