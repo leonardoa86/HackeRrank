da74aec41b7aba4be3b87ae7d3d8714fe88ad187
Leonardo Lima <leoamadori86@gmail.com>
2017-01-18 14:22:37 -0600

    Create staircase_three.c
    Display a pyramid as a staircase

---
+++ staircase_three.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+int n,row,Colspace,ColStar;
+
+printf("Enter number of rows to be printed\n");
+scanf("%d",&n);
+  for(row = 1; row <= n; row++){
+        for(Colspace = 1; Colspace <= n - row; Colspace++){
+               printf(" ");
+        }
+        for(ColStar = 1; ColStar <= (row*2)-1; ColStar++){
+               printf("*");
+        }
+      printf("\n");
+    }
+getch();
+ return 0;
+}
